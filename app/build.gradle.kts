435d9422e6ba4b23d222bb7bde7fcd7686f9ce06
DanielLucas23 <168872001+DanielLucas23@users.noreply.github.com>
2024-05-08 12:48:36 -0500

    Main Activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -33,15 +34,31 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
+    implementation("com.beust:klaxon:5.5")
+
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation(libs.firebase.auth)
+    implementation("com.google.firebase:firebase-firestore-ktx")
+
+    //Integrar el mapa de Google
+    implementation("com.google.maps.android:maps-ktx:3.2.0")
+    implementation("com.google.maps.android:maps-utils-ktx:3.2.0")
+    implementation("com.google.maps.android:android-maps-utils:2.2.3")
+    implementation("com.google.android.gms:play-services-maps:18.0.2")
+    implementation("com.google.android.gms:play-services-location:20.0.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/systemDK/busunheval/providers/AuthProvider.kt
@@ -0,0 +1,5 @@
+package com.systemDK.busunheval.providers
+
+class AuthProvider {
+
+}
